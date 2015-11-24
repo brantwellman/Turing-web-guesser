8eb812c0c1d8595692028ed04d83444af5aa02d6
Brant Wellman <BrantWellman@gmail.com>
2015-11-24 10:21:45 -0700

    IT4: Extensionn - Guess Limiting. Implemented and functional, but had to remove using the set method to set secret number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,27 +1,49 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :secret_number, rand(100)
+@@remaining_guesses = 5
+@@secret_number = rand(100)
+# set :secret_number, rand(100)
 get '/' do
   guess = params["guess"]
-  message = check_guess(guess)
+  guess_mess = guess_count(guess)
+  message = check_guess(guess) + guess_mess
   color = background_color(message)
-  erb :index, :locals => {:message => message, :color => color, :guess => guess}
+  erb :index, :locals => {:secret_number => @@secret_number, :message => message, :color => color, :guess => guess}
+end
+
+def guess_count(guess)
+  @@remaining_guesses -= 1 unless guess.nil?
+  if @@remaining_guesses == 0
+    start_over
+    " Sorry, you ran out of guesses. A new number has been generated for you to guess"
+  else
+    ""
+  end
+end
+
+def start_over
+  @@remaining_guesses = 5
+  @@secret_number = rand(100)
+  # set :secret_number, rand(100)
 end
 
 def check_guess(guess)
-  if guess.nil?
-    return message = "Please provide a number."
-  end
+  return "Please provide a number." if guess.nil?
   guess = guess.to_i
-  secret_num = settings.secret_number
-  if guess > secret_num
-    (guess - 5 > secret_num) ? message = "Woah! That is waaay too high!" : message = "Sorry. Your guess is too high"
-  elsif guess < secret_num
-    guess + 5 < secret_num ? message = "Hey now. That is waaay too low!" : message = "Sorry. That guess is too low"
-  elsif secret_num == guess
-    message = "Congrats! You beat the number guesser! The secret number is #{secret_num}"
+  # secret_num = settings.secret_number
+  if guess > @@secret_number
+    (guess - 5 > @@secret_number) ? "Woah! That is waaay too high!" + guesses_left : "Sorry. Your guess is too high" + guesses_left
+  elsif guess < @@secret_number
+    guess + 5 < @@secret_number ? "Hey now. That is waaay too low!" + guesses_left : "Sorry. That guess is too low" + guesses_left
+  elsif @@secret_number == guess
+    start_over
+    "Congrats! You beat the number guesser! The secret number is #{@@secret_number}.\nThe secret number has been reset. Try again." + guesses_left
   end
+end
+
+def guesses_left
+  " You have #{@@remaining_guesses} guesses left."
 end
 
 def background_color(message)
