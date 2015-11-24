d094a28c91ac0a31ce0df62bc692467bc762e74c
Brant Wellman <BrantWellman@gmail.com>
2015-11-23 23:40:48 -0700

    IT4: Basic color functionality working

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,17 +5,35 @@ set :secret_number, rand(100)
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:message => message}
+  color = background_color(message)
+  erb :index, :locals => {:message => message, :color => color, :guess => guess}
 end
 
 def check_guess(guess)
+  # if guess.nil?
+  #   message = "Please provide a number."
+  # end
   guess = guess.to_i
   secret_num = settings.secret_number
+  # if guess.nil?
+  #   message = "Please provide a number."
   if guess > secret_num
-    (guess - 5 > secret_num) ? message = "Woah! That is waaay to high!" : message = "So sorry. Your guess is too high"
+    (guess - 5 > secret_num) ? message = "Woah! That is waaay too high!" : message = "Sorry. Your guess is too high"
   elsif guess < secret_num
-    guess + 5 < secret_num ? message = "Hey now. You are guessing waay too low!" : message = "Nope. That guess is too low"
+    guess + 5 < secret_num ? message = "Hey now. That is waaay too low!" : message = "Sorry. That guess is too low"
   elsif secret_num == guess
     message = "Congrats! You beat the number guesser! The secret number is #{secret_num}"
+  end
+end
+
+def background_color(message)
+  if message.split.include?("waaay")
+    "#E33512"
+  elsif message.split.include?("Sorry.")
+    "#FCA395"
+  elsif message.split.include?("Congrats!")
+    "#169E1B"
+  # elsif message.split.include?("Please")
+  #   "#A3C9CF"
   end
 end
